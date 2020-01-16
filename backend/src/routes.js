12aaa4bb9f5d7daa67da5d7a7a4f52973a8dc729
Guilherme Revolta <guilhermerevolta@gmail.com>
2020-01-15 22:26:16 -0300

    add: Configure mongoose and add route file

---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+
+const routes = Router()
+
+routes.get('/', (request, response) => {
+  return response.json({ message: "Hello Ominstack "})
+})
+
+module.exports = routes
